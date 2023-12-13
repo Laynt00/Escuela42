96bfbd94c3163bedb1535d6775a49b532e5dceaa
laynt_42 <laynt00@gmail.com>
2023-12-13 12:30:45 +0100

    libft_printf

--- Cursus/ring_1/ft_printf.c
+++ Cursus/ring_1/ft_printf.c
@@ -6,7 +6,27 @@
 /*   By: alvartor <alvartor@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/13 12:10:36 by alvartor          #+#    #+#             */
-/*   Updated: 2023/12/13 12:10:42 by alvartor         ###   ########.fr       */
+/*   Updated: 2023/12/13 12:30:02 by alvartor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+int ft_isalpha(int  c)
+{
+    if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+        return (1);
+    else
+        return (0);
+}
+
+
+// #include <stdio.h>
+// #include <ctype.h>
+
+// int main()
+// {
+//     int c = 'a';
+//     printf("Funcion original: %d\n", isalpha(c));
+//     printf("Mi funcion: %d\n",ft_isalpha(c));
+
+//     return (0);
+// }
