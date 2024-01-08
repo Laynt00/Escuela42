ffaa726f921fc884fa9229b3bf8e6192aa577665
laynt_42 <laynt00@gmail.com>
2024-01-08 13:56:17 +0100

    empezar memchr

---
+++ Cursus/Libft/ft_memchr.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alvartor <alvartor@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/08 13:53:12 by alvartor          #+#    #+#             */
+/*   Updated: 2024/01/08 13:56:01 by alvartor         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// The memchr() function locates the first occurrence of c (converted to an unsigned
+//      char) in string s.
+
+// RETURN VALUES
+//      The memchr() function returns a pointer to the byte located, or NULL if no such byte
+//      exists within n bytes.
+
+#include <string.h>
+
+void	*memchr(const void *s, int c, size_t n)
+{
+	
+}
