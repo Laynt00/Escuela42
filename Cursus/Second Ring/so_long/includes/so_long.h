87f27f492a4b81b43bf1799723153f6206d1ff8c
laynt_42 <laynt00@gmail.com>
2024-05-16 12:05:55 +0200

    Building makefile

---
+++ Cursus/Second Ring/so_long/includes/so_long.h
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alvartor <alvartor@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/16 11:54:46 by alvartor          #+#    #+#             */
+/*   Updated: 2024/05/16 12:05:26 by alvartor         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# include <stdlib.h>
+
+# include "../libft/includes/libft.h"
+
+#  include "../mlx/mlx.h"
+#  define OS "macos"
+#  define LETTER_KEY_LEFT       0
+#  define LETTER_KEY_RIGHT      2
+#  define LETTER_KEY_UP 		13
+#  define LETTER_KEY_DOWN		1
+#  define LETTER_KEY_ACTION		14
+#  define ARROW_KEY_LEFT		123
+#  define ARROW_KEY_RIGHT		124
+#  define ARROW_KEY_UP			126
+#  define ARROW_KEY_DOWN		125
+#  define SPACE_BAR				49
+#  define KEY_ESC				53
+# endif
+
+# define WINDOW_TITLE "so_long"
+# define WINDOW_WIDTH 1366
+# define WINDOW_HEIGHT 768
+# define NB_LIFE 7
+# define HUD_LM 920
+
+# define BPX 100
+# define MPX 8
+# define GPX 14
+# define DPX 20
+# define JPX 40
+# define HPX 15
+# define EHPX 25
