a524d455bcb16d0a2bffb046abf7a05891052c88
Laynt <145455308+Laynt00@users.noreply.github.com>
2025-03-04 12:22:00 +0100

    Actualización ejercicios examen

---
+++ Practice Exam/04/ft_itoa.c
@@ -0,0 +1,38 @@
+static int	ft_len(int nbr)
+{
+	int len;
+
+	len = 0;
+	len = (nbr <= 0 ? 1 : 0);
+	while (nbr != 0)
+	{
+		nbr = nbr / 10;
+		len++;
+	}
+	return (len);
+}
+
+char		*ft_itoa(int n)
+{
+	unsigned int	nbr;
+	int				sign;
+	int				len;
+	char			*alpha;
+
+	sign = (n < 0 ? 1 : 0);
+	alpha = 0;
+	len = ft_len(n);
+	nbr = (n < 0 ? -n : n);
+	if ((alpha = malloc(sizeof(char) * len + 1)) == 0)
+		return (0);
+	alpha[len--] = '\0';
+	while (len >= 0)
+	{
+		alpha[len] = nbr % 10 + '0';
+		nbr /= 10;
+		len--;
+	}
+	if (sign == 1)
+		alpha[0] = '-';
+	return (alpha);
+}
