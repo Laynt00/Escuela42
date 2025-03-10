091130cc5fbfeb916e98cf434c404a06066fb51b
Laynt_Ubuntu_laptop <laynt00@gmail.com>
2025-03-10 13:49:40 +0100

    correccion and add

--- Practice Exam/02/hidenp.c
+++ Practice Exam/02/hidenp.c
@@ -6,19 +6,13 @@ int main(int ac, char *av[])
     
     if (ac == 3)
     {
-        // Looping over s2 only if there is something
-        // in s1
         while (av[2][j] && av[1][i])
         {
-            // If current char of s2 equals current char
-            // of s1, advance in s1
             if (av[2][j] == av[1][i])
                 i++;
-            // advance in s2 every time
             j++;
         }
-        // if we reached the end of s1, it's hidden in s2
-        if (av[1][i] == 0)
+        if (!av[1][i])
             write(1, "1", 1);
         else
             write(1, "0", 1);
